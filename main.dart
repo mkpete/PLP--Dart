061a7b89d9ddd598c40272953195f223cf59c01c
Muturi_Pete <67792341+mkpete@users.noreply.github.com>
2024-02-24 20:28:39 +0300

    Intro

---
+++ main.dart
@@ -0,0 +1,12 @@
+// Example code intro for Dart programming
+void main() {
+  String name = "Pete";
+  int age = 35;
+  String nationality = "Kenyan";
+  bool issingle = true;
+
+  print(name);
+  print(age);
+  print(nationality);
+  print(issingle);
+}
